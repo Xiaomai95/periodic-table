85e405442984b73350d315aae620b6082db107dc
Xiao Mai <ethanmyers1995@gmail.com>
2024-10-25 09:25:02 +0000

    fix: main menu now working

--- element.sh
+++ element.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 #When file runs: Please provide an element as an argument.
-if [[ -z $1 ]]
+MAIN_MENU() {
+  if [[ -z $1 ]]
   then
   echo "Please provide an element as an argument."
-fi
+  return
+  else
+  CHECK_DATABASE
+  fi
+}
 
+CHECK_DATABASE() {
+  CHECK_DATABASE_FOR_ARGUMENT=null
+  CHECK_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number=$1")
+  echo $CHECK_ATOMIC_NUMBER
+  #if argument doesn't exist in database, print error message: I could not find that element in the database.
+}
 
-#if argument doesn't exist in database, print error message: I could not find that element in the database.
-
+MAIN_MENU
