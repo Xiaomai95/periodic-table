8b08d87350f6d77bf5710e25d5d282a750ba7b72
Xiao Mai <ethanmyers1995@gmail.com>
2024-10-25 08:15:11 +0000

    feat: added if statement, if no argument given

--- element.sh
+++ element.sh
@@ -2,7 +2,12 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-#if no arguments given, print error message: Please provide an element as an argument.
+#When file runs: Please provide an element as an argument.
+if [[ -z $1 ]]
+  then
+  echo "Please provide an element as an argument."
+fi
+
 
 #if argument doesn't exist in database, print error message: I could not find that element in the database.
 
