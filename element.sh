32154470dfcf6ac60fbd783f6f522389ea133893
Xiao Mai <ethanmyers1995@gmail.com>
2024-10-25 08:09:48 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+#if no arguments given, print error message: Please provide an element as an argument.
+
+#if argument doesn't exist in database, print error message: I could not find that element in the database.
+
